5b78de1c9de2452cffd228c24a6d2137b15d49dc
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-02-29 16:39:58 -0500

    re-add components for testing

--- src/api/components.ts
+++ src/api/components.ts
@@ -1,6 +1,8 @@
 import { ListView } from "ui/list";
 import { Lit, Markdown } from "ui/markdown";
 import { TableView } from "ui/table";
+import { Card } from "ui/card";
+import { Embed } from "ui/embed";
 
 /** Provides convienent access to all of the datacore react components. */
 export const COMPONENTS = {
@@ -12,4 +14,6 @@ export const COMPONENTS = {
     Literal: Lit,
     /** Renders markdown using the Obsidian markdown renderer. */
     Markdown: Markdown,
+		Card,
+		Embed
 };
