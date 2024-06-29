6cb896ca52a9f4198f3b025dd3171070ba21d251
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-06-29 19:47:16 -0400

    incorporate `ScriptCache` into local api

--- src/api/local-api.tsx
+++ src/api/local-api.tsx
@@ -91,6 +91,8 @@ export class DatacoreLocalApi {
      * return { MyElement };
      * ```
      */
+
+
     public async require(path: string | Link): Promise<any> {
         const result = await this.scriptCache.load(path, { dc: this });
         return result.orElseThrow();
