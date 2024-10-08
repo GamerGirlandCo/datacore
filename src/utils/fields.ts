d0628e605c8339789e0780698d396d1387291a36
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <i.am.the.tablet@proton.me>
2024-10-08 16:13:25 -0400

    remove redundant `rewriteFieldInFile` function in favour of `editProvenance`

--- src/utils/fields.ts
+++ src/utils/fields.ts
@@ -1,48 +1,22 @@
 import { Field } from "expression/field";
-import { Literal, Literals } from "expression/literal";
+import { Literal } from "expression/literal";
 import { setInlineField } from "index/import/inline-field";
 import { MarkdownTaskItem } from "index/types/markdown";
 import { App } from "obsidian";
 import { Dispatch, useCallback, useContext } from "preact/hooks";
-import { APP_CONTEXT, DATACORE_CONTEXT } from "ui/markdown";
+import { APP_CONTEXT } from "ui/markdown";
 import { rewriteTask } from "./task";
 import { EditableAction } from "ui/fields/editable";
 import { Datacore } from "index/datacore";
-
-export async function rewriteFieldInFile(field: Field, newValue: Literal, app: App, core: Datacore) {
-    switch (field.provenance?.type) {
-			case "frontmatter": {
-					const tFile = app.vault.getFileByPath(field.provenance.file);
-            if (!tFile) return;
-            await app.fileManager.processFrontMatter(tFile, (fm) => {
-                fm[field.key] = newValue;
-            });
-						core.reload(tFile);
-            break;
-        }
-        case "inline-field": {
-            const tFile = app.vault.getFileByPath(field.provenance.file);
-            if (!tFile) return;
-            const content = await app.vault.read(tFile);
-            const lines = content.split(/\r?\n?/);
-            const line = lines[field.provenance.line];
-            const newLine = setInlineField(line, field.key, Literals.toString(newValue));
-            lines[field.provenance.line] = newLine;
-            await app.vault.modify(tFile, lines.join("\n"));
-						core.reload(tFile);
-						break;
-        }
-    }
-}
+import { editProvenance } from "index/edit/field";
 
 export function useSetField<T extends Literal>(field: Field, onChange?: (newValue: T) => void) {
     const app = useContext(APP_CONTEXT);
-		const core = useContext(DATACORE_CONTEXT);
     return useCallback(
         (newValue: T) => {
-            rewriteFieldInFile(field, newValue, app, core).then(() => {
-                if (onChange) onChange(newValue);
-            });
+						editProvenance(app, field.provenance!, newValue).then(() => {
+							if (onChange) onChange(newValue);
+						}); 
         },
         [field, onChange]
     );
